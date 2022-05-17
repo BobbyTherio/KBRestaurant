048ed3181e9128ea6dd727fadb71eb68d439412f
Bobby <98438695+BobbyTherio@users.noreply.github.com>
2022-05-17 13:57:11 -0600

    Delete config.js

--- Backend/config.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const config = new Sequelize('kbrestaurant', 'bobby', '=o@i<Z-x1)ox#r$+', {
-    host: '34.83.241.31',
-    dialect: 'mysql',
-});
-
-module.exports = config;
