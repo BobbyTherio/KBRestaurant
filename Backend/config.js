8c19aef4b3b7748e56dbe300817980dcb868b272
BobbyTherio <bobby2theriault@gmail.com>
2022-05-17 19:03:09 -0600

    update

--- Backend/config.js
+++ Backend/config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const config = new Sequelize('kbrestaurants', 'bobby', '=o@i<Z-x1)ox#r$+', {
+const config = new Sequelize('kbrestaurant', 'bobby', '=o@i<Z-x1)ox#r$+', {
     host: '34.83.241.31',
     dialect: 'mysql',
 });
