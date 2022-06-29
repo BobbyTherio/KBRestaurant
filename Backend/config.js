c6b8cf7c0c6ed8d789cbeaed62de1b90495682c4
Bobby <98438695+BobbyTherio@users.noreply.github.com>
2022-06-28 21:04:16 -0600

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const config = new Sequelize("kbrestaurant", "bobby", "5AR)^Ro@<hi7C1Pf", {
-    host: '34.83.241.31',
+const config = new Sequelize("kbrestaurant", "username", "password", {
+    host: '255.255.255.255',
     dialect: 'mysql'});
 
 module.exports = config;
